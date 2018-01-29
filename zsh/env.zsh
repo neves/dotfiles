0bee6e49fc799aff0219aca5dde4e835ba5127c9
Marcos Neves <marcos.neves@gmail.com>
2018-01-29 05:52:53 -0200

    Add bin project folder securitly to path

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,6 +4,10 @@ PATH="/usr/local/sbin:$PATH"
 # dotfiles bin
 PATH="$DOTFILES/bin:$PATH"
 
+# PWD/bin https://github.com/thoughtbot/dotfiles/blob/master/zsh/configs/post/path.zsh#L10 
+PATH=".git/safe/../../bin:$PATH"
+export -U PATH
+
 if [[ -n $SSH_CONNECTION ]]; then
   export EDITOR='vim'
 else
