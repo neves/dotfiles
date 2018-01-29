e870e1bda4ec9496d76542423fb0ca98261f6338
Marcos Neves <marcos.neves@gmail.com>
2018-01-29 05:58:35 -0200

    Reduce KeyRepeat delay

--- osx_defaults.sh
+++ osx_defaults.sh
@@ -9,7 +9,7 @@ clear
 # Keyboard - Set a fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
 # Keyboard - Set a fast keyboard start repeat
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 # Keyboard - Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 # Keyboard - Enable keyboard access for all controls
