71f287678fac470b178803d6fc1044e760b9b509
Marcos Neves <marcos.neves@gmail.com>
2018-12-05 22:36:15 -0200

    Update novo-mac.sh

--- novo-mac.sh
+++ novo-mac.sh
@@ -1,3 +1,5 @@
+# Configurar trackpad e drag em acessibilidade
+
 # Keyboard - Set a fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
 # Keyboard - Set a fast keyboard start repeat
@@ -6,3 +8,12 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 15
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 # Keyboard - Enable keyboard access for all controls
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+
+# executar git apenas para abrir popup para instalar DevTools
+git
+
+# instalar homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install visual-studio-code
